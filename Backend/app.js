a3365e057dc607ebb47d78617bbe87a684b16cbf
CosmiHub <achukmalhotra.gmail.com>
2025-05-31 10:38:43 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('welcome to the backendserver');
+})
+
+
+module.exports = app;
